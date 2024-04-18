0b017d180b3f9884523b91b2f33d49a0e16181d7
magwoo <magwood51@yandex.ru>
2024-04-18 22:52:42 +0300

    added simple rsx tests

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,3 +10,28 @@ pub mod prelude {
     pub use crate::macros::rsx;
     pub use cercis_html;
 }
+
+#[cfg(test)]
+mod rsx_tests {
+    use super::prelude::*;
+
+    #[test]
+    fn simple_tag() {
+        let correct = "<h1>Hello World!</h1>";
+        let render = rsx!(h1 { "Hello World!" }).render();
+
+        assert_eq!(render, correct)
+    }
+
+    #[test]
+    fn nested_tag() {
+        let correct = "<div><h1>Hello</h1><h2>World!</h2></div>";
+        let render = rsx!(div {
+            h1 { "Hello" }
+            h2 { "World!" }
+        })
+        .render();
+
+        assert_eq!(render, correct)
+    }
+}
