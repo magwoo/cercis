1ec319f7e3842f569783b6e9959db62b2f50d777
magwoo <magwood51@yandex.ru>
2024-04-29 18:35:58 +0300

    added more single tags support

--- rsx/src/node/element/single.rs
+++ rsx/src/node/element/single.rs
@@ -1,1 +1,4 @@
-pub const SINGLE_TAGS: [&str; 1] = ["link"];
+pub const SINGLE_TAGS: [&str; 14] = [
+    "area", "base", "br", "col", "embed", "hr", "img", "input", "link", "menuitem", "meta",
+    "param", "source", "track",
+];
