eba9490f95bb659123d96c955cc9e754d231dcff
magwoo <magwood51@yandex.ru>
2024-05-02 22:08:05 +0300

    added core docs examples

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,69 @@
+/*!
+# Template html at rust code
+
+Read more about macros at [`crate::macros`]
+
+# Examples
+
+## Render template to string
+
+```
+use cercis::prelude::*;
+
+let age = 19;
+let page = rsx!(h1 { "Your age is {age}" });
+
+let output = "<h1>Your age is 19</h1>";
+
+assert_eq!(page.render(), output)
+```
+
+## For loop and if
+
+```
+use cercis::prelude::*;
+
+let nums = vec![1, 2, 3];
+let page = rsx!(ul {
+    for num in nums {
+        li { "{num}" }
+        if num == 2 {
+            span { "num is 2" }
+        }
+    }
+});
+
+let output = "<ul><li>1</li><li>2</li><span>num is 2</span><li>3</li></ul>";
+
+assert_eq!(page.render(), output)
+```
+
+## Components
+
+```
+use cercis::prelude::*;
+
+let page = rsx!(div {
+    Button { text: "prev" }
+    Button { text: "next" }
+});
+
+let output = "<div><button class='component-button'>prev</button><button class='component-button'>next</button></div>";
+
+assert_eq!(page.render(), output);
+
+#[component]
+fn Button<'a>(text: &'a str, children: Element) -> Element {
+    rsx!(button {
+        class: "component-button",
+
+        "{text}"
+        children
+    })
+}
+```
+*/
+
 pub use typed_builder;
 
 #[cfg(test)]
