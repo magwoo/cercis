45337f5f7e0572246eb42a88f51bfb260c70693b
magwoo <magwood51@yandex.ru>
2024-05-05 13:25:42 +0300

    update macro module docs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -69,65 +69,63 @@ pub use typed_builder;
 #[cfg(test)]
 mod tests;
 
-/*
-Using macros ```rsx!()``` and ```#[component]```, you can create HTML pages directly in the Rust language
-
-# Examples
-
-## Full page
-
-```
-use cercis::prelude::*;
-
-// declarate people names
-let names = vec!["Boris", "Polina", "Igor", "Nikita"];
-
-// create peoples ordered list
-let peoples = rsx!(ol {
-    for name in names {
-        li { "{name}" }
-    }
-});
-
-// create full page using our Page component
-let page = rsx!(Page {
-    title: "Cercis",
-
-    h1 { "Peoples:" }
-    // insert element from peoples variable
-    peoples
-});
-
-// render our page to string
-println!("{}", page.render())
-
-#[component]
-pub fn Page<'a>(title: Option<&'a str>, children: Element) -> Element {
-    const META_CONTENT: &str = "witdh=device-width, initial-scale=1.0";
-
-    rsx!(
-        // turn into: <!DOCTYPE html>
-        doctype {}
-        html {
-            head {
-                meta { charset: "UTF-8" }
-                meta {
-                    name: "viewport",
-                    // interpolate META_CONTENT const into value like format!() macro
-                    content: "{META_CONTENT}",
-                }
-                // declarate title if exists
-                if let Some(title) = title {
-                    title { "{title}" }
-                }
-            }
-            body { children }
-        }
-
-    )
-}
-```
-*/
+/// Using macros ```rsx!()``` and ```#[component]```, you can create HTML pages directly in the Rust language
+///
+/// # Examples
+///
+/// ## Full page
+///
+/// ```
+/// use cercis::prelude::*;
+///
+/// // declarate people names
+/// let names = vec!["Boris", "Polina", "Igor", "Nikita"];
+///
+/// // create peoples ordered list
+/// let peoples = rsx!(ol {
+///     for name in names {
+///         li { "{name}" }
+///     }
+/// });
+///
+/// // create full page using our Page component
+/// let page = rsx!(Page {
+///     title: "Cercis",
+///
+///     h1 { "Peoples:" }
+///     // insert element from peoples variable
+///     peoples
+/// });
+///
+/// // render our page to string
+/// println!("{}", page.render())
+///
+/// #[component]
+/// pub fn Page<'a>(title: Option<&'a str>, children: Element) -> Element {
+///     const META_CONTENT: &str = "witdh=device-width, initial-scale=1.0";
+///
+///     rsx!(
+///         // turn into: <!DOCTYPE html>
+///         doctype {}
+///         html {
+///             head {
+///                 meta { charset: "UTF-8" }
+///                 meta {
+///                     name: "viewport",
+///                     // interpolate META_CONTENT const into value like format!() macro
+///                     content: "{META_CONTENT}",
+///                 }
+///                 // declarate title if exists
+///                 if let Some(title) = title {
+///                     title { "{title}" }
+///                 }
+///             }
+///             body { children }
+///         }
+///
+///     )
+/// }
+/// ```
 pub mod macros {
     pub use cercis_component::component;
     pub use cercis_rsx::rsx;
