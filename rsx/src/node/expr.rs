b7f55021f9294bfa53c053424a9e0342037dcb0a
magwoo <magwood51@yandex.ru>
2024-05-27 21:47:07 +0300

    added else if/else expr

--- rsx/src/node/expr.rs
+++ rsx/src/node/expr.rs
@@ -8,6 +8,8 @@ use crate::BodyCall;
 pub struct IfExpr {
     cond: Vec<TokenTree>,
     then_branch: BodyCall,
+    else_if_branch: Option<Box<Self>>,
+    else_branch: Option<BodyCall>,
 }
 
 impl Parse for IfExpr {
@@ -29,7 +31,28 @@ impl Parse for IfExpr {
 
         let then_branch = block.parse::<BodyCall>()?;
 
-        Ok(Self { cond, then_branch })
+        let (else_branch, else_if_branch) = match input.peek(Token![else]) {
+            true if input.peek2(Token![if]) => {
+                input.parse::<Token![else]>()?;
+
+                (None, Some(Box::new(input.parse::<Self>()?)))
+            }
+            true => {
+                input.parse::<Token![else]>()?;
+                let block;
+                braced!(block in input);
+
+                (Some(block.parse::<BodyCall>()?), None)
+            }
+            false => (None, None),
+        };
+
+        Ok(Self {
+            cond,
+            then_branch,
+            else_if_branch,
+            else_branch,
+        })
     }
 }
 
@@ -37,11 +60,24 @@ impl ToTokens for IfExpr {
     fn to_tokens(&self, tokens: &mut TokenStream2) {
         let cond = self.cond.as_slice();
         let then_branch = &self.then_branch;
+        let else_if_branch = &self.else_if_branch;
+        let else_branch = &self.else_branch;
+
+        let else_if_tokens = match else_if_branch {
+            Some(else_if_branch) => quote!(else #else_if_branch),
+            None => quote!(),
+        };
+
+        let else_tokens = match else_branch {
+            Some(then_branch) => quote!(else { #then_branch }),
+            None if else_if_branch.is_some() => quote!(),
+            None => quote!(else { VBody::new() }),
+        };
 
         quote!({
-            let mut body = VBody::new();
-            if #(#cond)* { body = #then_branch }
-            body
+            if #(#cond)* { #then_branch }
+            #else_if_tokens
+            #else_tokens
         })
         .to_tokens(tokens)
     }
