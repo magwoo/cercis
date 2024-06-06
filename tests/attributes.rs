294362f0985057775c57c58037c1220345fdd78c
magwoo <magwood51@yandex.ru>
2024-06-06 19:44:32 +0300

    added more tests for attributes

--- tests/attributes.rs
+++ tests/attributes.rs
@@ -21,6 +21,37 @@ fn multiple_attrs() {
 }
 
 #[test]
+fn single_tag_attr() {
+    let correct = "<input placeholder='username'>";
+    let render = rsx!(input {
+        placeholder: "username"
+    })
+    .render();
+
+    assert_eq!(render, correct)
+}
+
+#[test]
+fn raw_attr_name() {
+    let correct = "<input type='text'>";
+    let render = rsx!(input { r#type: "text" }).render();
+
+    assert_eq!(render, correct)
+}
+
+#[test]
+fn single_tag_multiple_attr() {
+    let correct = "<input placeholder='some number' type='number'>";
+    let render = rsx!(input {
+        placeholder: "some number",
+        r#type: "number"
+    })
+    .render();
+
+    assert_eq!(render, correct)
+}
+
+#[test]
 fn fmt_attr() {
     let class = "container";
 
